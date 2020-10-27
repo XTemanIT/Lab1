895536bf76ef8587b67012df1f1ffd3adb0e1385
XTemanIT <59162156+XTemanIT@users.noreply.github.com>
2020-10-27 18:47:03 +0200

    Lab1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab1CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V, h, r;
+            double Pi = 3.14;
+            Console.WriteLine("Enter height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter r: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            V = (h / 3) * Pi * Math.Pow(r, 2);
+            Console.WriteLine($"V = {V}");
+            Console.ReadKey();
+        }
+    }
+}
